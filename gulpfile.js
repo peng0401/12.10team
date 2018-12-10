02bd9574ed52392cfca1866b32c5d4dbed3becda
peng0401 <814162169@qq.com>
2018-12-10 20:44:38 +0800

    前端完成

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@ var gulp = require("gulp")
 
 var server = require("gulp-webserver")
 
+var sass = require("gulp-sass");
+//起服务
 gulp.task('server',() => {
     return gulp.src("src")
     .pipe(server({
@@ -14,3 +16,15 @@ gulp.task('server',() => {
         ]
     }))
 })
+
+gulp.task('sass',() => {
+    return gulp.src("./src/scss/*.scss")
+    .pipe(sass())
+    .pipe(gulp.dest("./src/css"))
+})
+
+gulp.task("watch",() => {
+    return gulp.watch("./src/scss/*.scss",gulp.series("sass"));
+})
+
+gulp.task("dev",gulp.series("sass","server",'watch'))
